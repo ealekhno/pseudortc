22ca5ded5034d41414e855c1e773b6da8bda368d
root <root@silverfish.phys-el.ru>
2020-10-06 10:17:18 +0300

    test.sh THERTC

--- tests/test.sh
+++ tests/test.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
+export THERTC=/dev/rtc1
+
 rmmod pseudortc 2>&1 >> /dev/null
 insmod ../module/pseudortc.ko || (echo "insmod failed"; exit 1)
 
 echo " "
 echo "setting hwclock to current time"
-hwclock -w -f /dev/rtc1 || (echo "set hwclock  failed"; exit 1)
+hwclock -w -f $THERTC || (echo "set hwclock  failed"; exit 1)
 echo "reading time"
-printf "our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 2
 echo "sleeping 10 seconds"
 sleep 8
-printf "after sleep our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "after sleep our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "after sleep system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 
 sleep 1
@@ -22,14 +24,14 @@ echo "going slower"
 echo "slow" > /proc/driver/rtcspeed
 echo " "
 echo "setting hwclock to current time"
-hwclock -w -f /dev/rtc1 || (echo "set hwclock  failed"; exit 1)
+hwclock -w -f $THERTC || (echo "set hwclock  failed"; exit 1)
 echo "reading time"
-printf "our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 2
 echo "sleeping 10 seconds"
 sleep 8
-printf "after sleep our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "after sleep our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "after sleep system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 1
 echo " "
@@ -39,30 +41,30 @@ echo "going faster"
 echo "fast" > /proc/driver/rtcspeed
 echo " "
 echo "setting hwclock to current time"
-hwclock -w -f /dev/rtc1 || (echo "set hwclock  failed"; exit 1)
+hwclock -w -f $THERTC || (echo "set hwclock  failed"; exit 1)
 echo "reading time"
-printf "our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 2
 echo "sleeping 10 seconds"
 sleep 8
-printf "after sleep our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "after sleep our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "after sleep system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 1
 echo " "
 
-echo "going south"
+echo "going random"
 echo "rand" > /proc/driver/rtcspeed
 echo " "
 echo "setting hwclock to current time"
-hwclock -w -f /dev/rtc1 || (echo "set hwclock  failed"; exit 1)
+hwclock -w -f $THERTC || (echo "set hwclock  failed"; exit 1)
 echo "reading time"
-printf "our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 2
 echo "sleeping 10 seconds"
 sleep 8
-printf "after sleep our clock is %s \n" "`hwclock -f /dev/rtc1`" & \
+printf "after sleep our clock is %s \n" "`hwclock -f $THERTC`" & \
 printf "after sleep system clock is %s \n" "`hwclock -f /dev/rtc0 `"
 sleep 1
 echo " "
